1af8391145f2fd25cade48b9786cd525bbe063f3
Pavel Tarakanov <pavel191094@icloud.com>
2025-09-27 06:58:25 +0300

    resize keyboard buttons

--- keyboards/common_keyboards.py
+++ keyboards/common_keyboards.py
@@ -4,12 +4,17 @@ from aiogram.types import KeyboardButton, ReplyKeyboardMarkup
 class ButtonText:
     HELLO = "Hello!"
     WHATS_NEXT = "What's next?"
+    BYE = "Bye-Bye!"
 
 
 def get_on_start_keyboard():
     button_hello = KeyboardButton(text=ButtonText.HELLO)
     button_help = KeyboardButton(text=ButtonText.WHATS_NEXT)
-    buttons_first_row = [button_hello]
-    buttons_second_row = [button_help]
-    markup = ReplyKeyboardMarkup(keyboard=[buttons_first_row, buttons_second_row])
+    button_bye = KeyboardButton(text=ButtonText.BYE)
+    buttons_first_row = [button_hello, button_help]
+    buttons_second_row = [button_bye]
+    markup = ReplyKeyboardMarkup(
+        keyboard=[buttons_first_row, buttons_second_row],
+        resize_keyboard=True,
+    )
     return markup
