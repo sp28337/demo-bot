3e26635ad6b471def95d53cd37c01997e18d4897
Pavel Tarakanov <pavel191094@icloud.com>
2025-09-29 12:56:47 +0300

    callback button with url

--- routers/callback_handlers/info_kb_callback_handler.py
+++ routers/callback_handlers/info_kb_callback_handler.py
@@ -1,3 +1,16 @@
-from aiogram import Router
+from random import randint
+
+from aiogram import Router, F
+from aiogram.types import CallbackQuery
+
+from keyboards.inline_keyboards.info_kb import random_cite_callback_data
 
 router = Router(name=__name__)
+
+
+@router.callback_query(F.data == random_cite_callback_data)
+async def handle_random_cite_cb(callback_query: CallbackQuery):
+    bot_me = await callback_query.bot.me()
+    await callback_query.answer(
+        url=f"t.me/{bot_me.username}?start={randint(1, 100)}",
+    )
--- routers/commands/base_commands.py
+++ routers/commands/base_commands.py
@@ -17,6 +17,7 @@ router = Router(name=__name__)
 @router.message(CommandStart())
 async def handle_start(message: types.Message):
     url = "https://www.citypng.com/public/uploads/preview/hd-python-logo-symbol-transparent-png-735811696257415dbkifcuokn.png"
+    print("Command text:", repr(message.text))
     await message.answer(
         text=f"{markdown.hide_link(url=url)}Hello, {markdown.hbold(message.from_user.full_name)}!",
         parse_mode=ParseMode.HTML,
