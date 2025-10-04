f3daa8548e288cab0c4022dd6cf738af5fd00d64
Pavel Tarakanov <pavel191094@icloud.com>
2025-10-04 07:30:56 +0300

    add inline kb for products btn

--- keyboards/inline_keyboards/shop_kb.py
+++ keyboards/inline_keyboards/shop_kb.py
@@ -1,20 +1,21 @@
-from enum import Enum, auto
+from enum import IntEnum, auto
 
 from aiogram.filters.callback_data import CallbackData
 from aiogram.types import InlineKeyboardMarkup
 from aiogram.utils.keyboard import InlineKeyboardBuilder
 
 
-class ShopActions(Enum):
+class ShopActions(IntEnum):
     products = auto()
     address = auto()
+    root = auto()
 
 
 class ShopCallbackData(CallbackData, prefix="shop"):
     actions: ShopActions
 
 
-def build_shop_keyboarb() -> InlineKeyboardMarkup:
+def build_shop_keyboard() -> InlineKeyboardMarkup:
     builder = InlineKeyboardBuilder()
     builder.button(
         text="Show products",
@@ -23,6 +24,16 @@ def build_shop_keyboarb() -> InlineKeyboardMarkup:
     builder.button(
         text="My address",
         callback_data=ShopCallbackData(actions=ShopActions.address).pack(),
+    )
+    builder.adjust(1)
+    return builder.as_markup()
+
+
+def build_products_keyboard() -> InlineKeyboardMarkup:
+    builder = InlineKeyboardBuilder()
+    builder.button(
+        text="Back to root",
+        callback_data=ShopCallbackData(actions=ShopActions.root).pack(),
     )
     builder.adjust(1)
     return builder.as_markup()
