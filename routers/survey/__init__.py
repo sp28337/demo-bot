eb84b8c185cdeef6a9599ca4085936bdae104ed4
Pavel Tarakanov <pavel191094@icloud.com>
2025-10-05 09:15:19 +0300

    FSM base

---
+++ routers/survey/__init__.py
@@ -0,0 +1,10 @@
+from aiogram import Router
+
+from .handlers import router as handlers_router
+
+
+router = Router(name="survey")
+
+router.include_routers(
+    handlers_router,
+)
---
+++ routers/survey/handlers.py
@@ -0,0 +1,35 @@
+from aiogram import Router, types, F
+from aiogram.filters import Command
+from aiogram.fsm.context import FSMContext
+from aiogram.utils import markdown
+
+from .states import Survey
+
+router = Router(name=__name__)
+
+
+@router.message(Command("survey", prefix="!/%"))
+async def handle_start_survey(
+    message: types.Message,
+    state: FSMContext,
+):
+    await state.set_state(Survey.full_name)
+    await message.answer("Welcoe to our weekly survey! What's your name?")
+
+
+@router.message(Survey.full_name, F.text)
+async def handle_survey_user_full_name(
+    message: types.Message,
+    state: FSMContext,
+):
+    await state.clear()
+    await message.answer(f"Hello, {markdown.hbold(message.text)}")
+
+
+@router.message(Survey.full_name)
+async def handle_survey_user_full_name_invalid_content_type(
+    message: types.Message,
+):
+    await message.answer(
+        f"Sorry, I don't understand, send your name as text.",
+    )
